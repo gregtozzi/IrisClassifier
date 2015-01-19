5680ffffa7af4d7aadd10ceabfe1ae653991e3ca
Greg Tozzi <gtozzi@alum.mit.edu>
2015-01-19 02:32:18 -0500

    Initial commit

---
+++ server.R
@@ -0,0 +1,96 @@
+library(shiny)
+library(rCharts)
+library(caret)
+
+inTrain <- createDataPartition(y = iris$Species,
+                               p = 0.7,
+                               list = FALSE)
+
+trainSet <- iris[inTrain, ]
+
+shinyServer(
+        function(input, output) {
+                
+                output$ui <- renderUI({
+                        if(length(input$coVars > 0)) {
+                                return(actionButton("Go", label = "Go"))
+                        }
+                        return(tags$b(style="color:red", "Select one or more covariate."))
+                })
+                
+                output$uiy <- renderUI({
+                        availVars <- names(iris)[which(!names(iris)[1:4] %in% input$xaxis)]
+                        return(selectInput("yaxis", "Choose y-axis",
+                                           choices = availVars,
+                                           selected = availVars[1]))
+                })
+                
+                output$uiy2 <- renderUI({
+                        availVars <- names(iris)[which(!names(iris)[1:4] %in% input$xaxis2)]
+                        return(selectInput("yaxis2", "Choose y-axis",
+                                           choices = availVars,
+                                           selected = availVars[1]))
+                })
+                
+                output$plot1 <- renderChart2({
+                        h1 <- hPlot(y = input$yaxis, x = input$xaxis, data = trainSet(),
+                                    type = "scatter",
+                                    group = "Species")
+                        h1$plotOptions(series = list(marker = list(symbol = "circle")))
+                        h1$chart(width = 400)
+                        h1
+                })
+                
+                inTrain <- reactive({
+                        set.seed(20)
+                        createDataPartition(y = iris$Species,
+                                            p = input$testTrain / 100,
+                                            list = FALSE)
+                })
+                
+                trainSet <- reactive({
+                        iris[inTrain(), ]
+                })
+                
+                testSet <- reactive({
+                        iris[-inTrain(), ]
+                })
+                
+                xSet <- reactive({
+                        trainSet()[, input$coVars]
+                })
+                
+                
+                modPredict <- reactive({
+                      modFit <- train(x = xSet(), y = trainSet()$Species,
+                            method = input$modelMethod)
+                      TestS <- testSet()
+                      predictRes <- predict(modFit, TestS)
+                      newDF <- cbind(TestS, predictRes)
+                      names(newDF) <- c(names(iris), "Prediction")
+                      return(newDF)
+                })
+                
+                #output$testVal <- renderText({ dim(xSet()) })
+                output$testVal <- renderText({ dim(modPredict()) })
+                
+                output$plot2 <- renderChart2({
+                        ModP <- modPredict()
+                        h2 <- hPlot(y = input$yaxis2,
+                                    x = input$xaxis2,
+                                    data = ModP,
+                                    type = "scatter",
+                                    group = "Prediction"
+                                    )
+                        falseGroup <- ModP[which(ModP$Species != ModP$Prediction), ]
+                        h2$data(y = falseGroup[, input$yaxis2],
+                                x = falseGroup[, input$xaxis2],
+                                type = "scatter",
+                                name = "False Predictions")
+                        h2$plotOptions(series = list(marker = list(symbol = "circle", radius = 6)))
+                        h2$chart(width = 400)
+                        h2
+                })
+
+        }
+        )
